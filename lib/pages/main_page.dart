703bbf0c1ba541b683b0781f99505a394d4eaa05
jmapps <jmappsmuslim@gmail.com>
2022-12-10 08:23:52 +0300

    Clear project to inherited widget lesson

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,65 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:my_test_app/pages/home_page.dart';
-import 'package:my_test_app/pages/server_page.dart';
-import 'package:my_test_app/pages/settings_page.dart';
 
 class MainPage extends StatefulWidget {
-  MainPage({Key? key}) : super(key: key);
+  const MainPage({Key? key}) : super(key: key);
 
   @override
   State<MainPage> createState() => _MainPageState();
 }
 
 class _MainPageState extends State<MainPage> {
-  int currentIndex = 1;
-
-  List<Widget> myWidgets = [
-    ServerPage(),
-    HomePage(),
-    SettingsPage(),
-  ];
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         centerTitle: true,
-        elevation: 0,
-        leading: Icon(
-          Icons.supervised_user_circle_rounded,
-          size: 35,
-        ),
-        title: Text(
-          'Верстка',
-        ),
-        backgroundColor: Colors.blueGrey.shade700,
+        title: Text('Test app'),
       ),
-      body: myWidgets[currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        elevation: 0,
-        currentIndex: currentIndex,
-        onTap: (index) {
-          setState(() {
-            currentIndex = index;
-          });
-        },
-        selectedItemColor: Colors.red,
-        unselectedItemColor: Colors.blueGrey,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_tree),
-            label: 'Server',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_filled),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          ),
-        ],
-      ),
+      body: Container(),
     );
   }
 }
--- lib/pages/root_page.dart
+++ lib/pages/root_page.dart
@@ -7,6 +7,10 @@ class RootPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Test app',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
       home: MainPage(),
     );
   }
